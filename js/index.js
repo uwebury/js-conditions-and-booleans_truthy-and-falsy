81f6c71b50c6f05b745d0a9731a6907bd819f1a6
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-15 16:10:56 +0100

    Entered expressions

--- js/index.js
+++ js/index.js
@@ -1,2 +1,35 @@
-const truthyOrFalsy = Boolean(3 >= 2);
+const truthyOrFalsy = Boolean(2 == "2");
 console.log("The expression is " + truthyOrFalsy);
+
+const truthyOrFalsy2 = Boolean(2 === "2");
+console.log("The expression is " + truthyOrFalsy2);
+
+const truthyOrFalsy3 = Boolean(2 === 2);
+console.log("The expression is " + truthyOrFalsy3);
+
+const truthyOrFalsy4 = Boolean("hello world");
+console.log("The expression is " + truthyOrFalsy4);
+
+const truthyOrFalsy5 = Boolean("");
+console.log("The expression is " + truthyOrFalsy5);
+
+const truthyOrFalsy6 = Boolean(true);
+console.log("The expression is " + truthyOrFalsy6);
+
+const truthyOrFalsy7 = Boolean(0);
+console.log("The expression is " + truthyOrFalsy7);
+
+const truthyOrFalsy8 = Boolean(-50);
+console.log("The expression is " + truthyOrFalsy8);
+
+const truthyOrFalsy9 = Boolean(50);
+console.log("The expression is " + truthyOrFalsy9);
+
+const truthyOrFalsy10 = Boolean(5 > 1);
+console.log("The expression is " + truthyOrFalsy10);
+
+const truthyOrFalsy11 = Boolean("1 > 10");
+console.log("The expression is " + truthyOrFalsy11);
+
+const truthyOrFalsy12 = Boolean("false");
+console.log("The expression is " + truthyOrFalsy12);
